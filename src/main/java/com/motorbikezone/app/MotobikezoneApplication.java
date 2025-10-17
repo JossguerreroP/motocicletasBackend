bcd28e5a6bd3f60dfcd173a3fa9977abcb54ac8c
JossguerreroP <josguerrerop@udistrital.edu.co>
2025-10-16 22:11:27 -0500

    Initial commit

---
+++ src/main/java/com/motorbikezone/app/MotobikezoneApplication.java
@@ -0,0 +1,13 @@
+package com.motorbikezone.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotobikezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotobikezoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motorbikezone/app/MotobikezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorbikezone.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotobikezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
