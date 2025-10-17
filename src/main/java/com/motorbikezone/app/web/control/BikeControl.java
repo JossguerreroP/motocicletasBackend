faf79f8d8e55ba246e7b917558cf0d117efc72ad
JossguerreroP <josguerrerop@udistrital.edu.co>
2025-10-17 09:35:35 -0500

    Correct Junit Test

--- src/main/java/com/motorbikezone/app/web/control/BikeControl.java
+++ src/main/java/com/motorbikezone/app/web/control/BikeControl.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.motorbikezone.app.services.BikeService;
+import com.motorbikezone.app.web.modelo.BikeDTO;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -16,5 +19,14 @@ import java.util.UUID;
 @RequestMapping("/bikes")
 @RestController
 public class BikeControl {
-
+	private final   BikeService bikeService;
+	
+	   public BikeControl(BikeService bikeService) {
+	        this.bikeService = bikeService;
+	    }
+	   
+	   @GetMapping
+	   public ResponseEntity<List<BikeDTO>> getAllBikes() {
+	        return new ResponseEntity<>(bikeService.getBikes(), HttpStatus.OK);
+	    }
 }
--- src/test/java/com/motorbikezone/app/MotobikezoneApplicationTests.java
+++ src/test/java/com/motorbikezone/app/MotobikezoneApplicationTests.java
@@ -1,13 +1,44 @@
 package com.motorbikezone.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.motorbikezone.app.services.BikeService;
+import com.motorbikezone.app.web.control.BikeControl;
+import com.motorbikezone.app.web.modelo.BikeDTO;
+
+@WebMvcTest(BikeControl.class)
 class MotobikezoneApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @MockitoBean
+    private BikeService bikeService;
+
+    @Test
+    void testBikes() throws Exception {
+        var bikes = List.of(
+            new BikeDTO(1, "Yamaha R1", 299, 20000, "yamaha.jpg", "Yamaha", "R1")
+        );
+
+        when(bikeService.getBikes()).thenReturn(bikes);
+
+        mockMvc.perform(get("/bikes"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].name").value("Yamaha R1"));
+    }
 }
+
+
+
